a6817664213230b9844d9cd20768be5c195560ae
Ren Mizutani <nightfall-immortal-longview@live.jp>
2015-06-25 22:46:32 +0900

    first commit

---
+++ LOSAlgorithm/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace LOSAlgorithm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Canvas = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.Sprit = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Sprit)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Canvas
+            // 
+            this.Canvas.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Canvas.Location = new System.Drawing.Point(4, 4);
+            this.Canvas.Name = "Canvas";
+            this.Canvas.Size = new System.Drawing.Size(593, 600);
+            this.Canvas.TabIndex = 0;
+            this.Canvas.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.Canvas.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.Canvas.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.Sprit);
+            this.panel2.Location = new System.Drawing.Point(603, 4);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(113, 600);
+            this.panel2.TabIndex = 1;
+            // 
+            // Sprit
+            // 
+            this.Sprit.Location = new System.Drawing.Point(14, 45);
+            this.Sprit.Maximum = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            this.Sprit.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.Sprit.Name = "Sprit";
+            this.Sprit.Size = new System.Drawing.Size(82, 19);
+            this.Sprit.TabIndex = 0;
+            this.Sprit.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.Sprit.ValueChanged += new System.EventHandler(this.Sprit_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "分割数";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(719, 610);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.Canvas);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Sprit)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel Canvas;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown Sprit;
+    }
+}
+
---
+++ LOSAlgorithm/Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LOSAlgorithm
+{
+    public partial class Form1 : Form
+    {
+
+        class Vertex
+        {
+            public Point v;
+
+            public bool Touched(Point p)
+            {
+                var dx = v.X - p.X;
+                var dy = v.Y - p.Y;
+
+                return dx * dx + dy * dy < 20 * 20;
+            }
+
+            public Vertex(Point p)
+            {
+                v = p;
+            }
+        }
+
+        class Cell
+        {
+           public Point p;
+
+            public Cell(Point v)
+            {
+                p = v;
+            }
+        }
+
+        int CanvasSize
+        {
+            get
+            {
+                return Math.Min(Canvas.Width, Canvas.Height) - 1;
+            }
+        }
+
+        Point lastTouched = new Point();
+
+        Vertex st, en;
+
+        Vertex selected;
+
+        List<Cell> Cells = new List<Cell>();
+
+        int SpritNum { get { return (int)Sprit.Value; } }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            st = new Vertex(new Point(50,50));
+            en = new Vertex(new Point(100, 50));
+        }
+
+        PointF PointToCell(Point p)
+        {
+
+            return new PointF(1.0f * p.X / CanvasSize * SpritNum, 1.0f * p.Y / CanvasSize * SpritNum);
+        }
+
+        Rectangle CellToRect(Point cell)
+        {
+            return new Rectangle(cell.X * CanvasSize / SpritNum, cell.Y * CanvasSize / SpritNum, CanvasSize / SpritNum, CanvasSize / SpritNum);
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            lastTouched = e.Location;
+
+            selected = null;
+
+            if (st.Touched(lastTouched))
+                selected = st;
+
+            if (en.Touched(lastTouched))
+                selected = en;
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (selected == null || e.Button != System.Windows.Forms.MouseButtons.Left)
+                return;
+
+            selected.v.X += e.X - lastTouched.X;
+            selected.v.Y += e.Y - lastTouched.Y;
+
+            lastTouched = e.Location;
+
+            Cells = LOS(PointToCell(st.v), PointToCell(en.v));
+            Canvas.Invalidate();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            g.Clear(Color.White);
+
+            foreach(var i in Enumerable.Range(0, SpritNum+1) )
+            {
+                Point p1 = new Point(i * CanvasSize / SpritNum, 0);
+                Point p2 = new Point(i * CanvasSize / SpritNum, CanvasSize);
+
+                g.DrawLine(Pens.Black, p1, p2);
+
+
+                Point l = new Point(0,i * CanvasSize / SpritNum);
+                Point r = new Point(CanvasSize, i * CanvasSize / SpritNum);
+
+                g.DrawLine(Pens.Black, l, r);
+            }
+;
+            if(Cells.Count > 0)
+                g.FillRectangles(new SolidBrush(Color.FromArgb(100, 25, 0, 0)), Cells.Select( (c) => CellToRect(c.p)).ToArray());
+
+            g.DrawLine(Pens.Red, st.v, en.v);
+
+            g.DrawString(Cells.Count + "", new Font("MsGothic", 12), Brushes.Red, new PointF());
+        }
+
+        private void Sprit_ValueChanged(object sender, EventArgs e)
+        {
+            Canvas.Invalidate();
+        }
+
+
+        private List<Cell> LOS(PointF st, PointF en)
+        {
+            bool bresenham = true;
+            List<Cell> res = new List<Cell>();
+
+            Point stInt = new Point((int)st.X,(int) st.Y);
+            Point enInt = new Point((int)en.X,(int) en.Y);
+
+            Point next = stInt;
+
+            PointF delta = new PointF(en.X - st.X, en.Y - st.Y);
+
+            Point step = new Point(delta.X < 0 ? -1 : 1, delta.Y < 0 ? -1 : 1);
+
+            delta.X = Math.Abs(delta.X);
+            delta.Y = Math.Abs(delta.Y);
+
+            PointF fmod = new PointF(Math.Abs(st.X - stInt.X), Math.Abs(st.Y - stInt.Y));
+
+            if (step.X < 0)
+                fmod.X = 1.0f - fmod.X;
+
+            if (step.Y < 0)
+                fmod.Y = 1.0f - fmod.Y;
+
+             res.Add(new Cell(next));
+            if(delta.X > delta.Y)
+            {
+                float fraction = -delta.X * (1 - fmod.Y) + delta.Y * (1 - fmod.X);
+
+                while(next.X != enInt.X)
+                {
+                    if(fraction >= 0)
+                    {
+                        next.Y += step.Y;
+                        fraction -= delta.X;
+                        if (Math.Abs(fraction) != delta.X && bresenham == false)
+                            res.Add(new Cell(next));
+                    }
+                    next.X += step.X;
+                    fraction += delta.Y;
+                    res.Add(new Cell(next));
+                }
+            }
+            else
+            {
+                float fraction = -delta.Y * (1 - fmod.X) + delta.X * (1 - fmod.Y);
+                while(next.Y != enInt.Y)
+                {
+                    if(fraction >= 0)
+                    {
+                        next.X += step.X;
+                        fraction -= delta.Y;
+                        if (Math.Abs(fraction) != delta.Y && bresenham == false)
+                            res.Add(new Cell(next));
+                    }
+                    next.Y += step.Y;
+                    fraction += delta.X;
+                    res.Add(new Cell(next));
+                }
+            }
+
+            if( res[res.Count-1].p != enInt)
+                 res.Add(new Cell(enInt));
+            return res;
+        }
+    }
+}
